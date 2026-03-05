86d8048884e8c695385540ee9f2597039e4a6248
iffatazriel <iffatazrielfitrianto@gmail.com>
2026-03-05 22:37:11 +0700

    Installing Database With Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,14 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "./generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+  globalForPrisma.prisma ??
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
